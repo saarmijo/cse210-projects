94dca8012b9a668e122aefb9f57da68a40aebca4
saarmijo <sofiaxarmijo@gmail.com>
2024-01-23 15:50:32 -0700

    W03 Prep Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "2010";
+        job1._endYear = "2020";
+
+        Job job2 = new Job();
+        job2._jobTitle = "IT Intern";
+        job2._company = "Apple";
+        job2._startYear = "2008";
+        job2._endYear = "2010";
+        
+        Resume myResume = new Resume();
+        myResume._name = "Sofia Armijo";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
