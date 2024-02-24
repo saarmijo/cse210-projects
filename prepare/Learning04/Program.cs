318fcf7c03a79f1eed5162a4668298a75d01188d
saarmijo <sofiaxarmijo@gmail.com>
2024-02-24 13:59:39 -0700

    Learning Activity Week 7

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Sofia Armijo", "Grammar");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment m1 = new MathAssignment("Sofia Armijo", "Fractions", "7.3", "8-19");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+
+        WritingAssignment w1 = new WritingAssignment("Sofia Armijo", "European History", "The Causes of World War II");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
